97cbf2d057927476344bc4615cb90560cf4d8ae7
Landon Ferneau <landon@example.com>
2019-08-30 19:27:04 -0400

    Updates the LICENCE, README.md, and completes the assignment.

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('こんにちは、私の名前はファーノーです! Hi, my name is Landon Ferneau!')
+print('My favorite games are Super Smash Bros. Ultimate, The World Ends With You, and almost anything Pokemon')
+print('My biggest concern with this class is that I have no prior coding experience, and I tend to procrastinate, which is something that I need to work on fixing')
+print('I am excited about meeting new people with similar interests and eventually getting married')
+print('My stackoverflow.com user number is 12001478')
+print('https://github.com/lferneau')
